7ab8f5adf34d8661827239218cb0190f9f57236f
federoss8 <federsrb@gmail.com>
2023-02-27 23:13:07 -0300

    cuarto commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const app = require('./server/server');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
+
+// routes
+
+app.get("/", (req, res) => {
+    res.send("Bienvenido a mi API")
+});
 
 app.listen(PORT, () => console.log('Server corriendo en ', PORT));
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const routes = require('../server/routes/routes')
 const app = express()
 
 app.use(express.json())
-app.use('/api', routes)
+app.use('/api', routes);
 
 module.exports = app;
