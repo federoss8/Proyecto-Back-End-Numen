98301453ce9c1747644953fb52a466a9b4111792
federoss8 <federsrb@gmail.com>
2023-03-09 20:47:33 -0300

    quinto commit

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userSchema = require('../models/userModels');
-//const productSchema = require('../models/productsModels');
+const productSchema = require('../models/productsModels');
 const router = express.Router();
 
 // crear usuario
@@ -13,8 +13,49 @@ router.post('/usuarios', (req, res) => {
 // crear producto
 
 router.post('/productos', (req, res) => {
-    res.send('crear producto');
+    const product = productSchema(req.body);
+    product.save().then((data) => res.json(data)).catch((error) => res.json({message: error}));
 });
 
+//obtener todos los usuarios
+
+router.get('/usuarios', (req, res) => {
+    userSchema.find().then((data) => res.json(data)).catch((error) => res.json({message: error}));
+});
+
+//obtener todos los productos
+
+router.get('/productos', (req, res) => {
+    productSchema.find().then((data) => res.json(data)).catch((error) => res.json({message: error}));
+});
+
+//obtener un usuario
+
+router.get('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    userSchema.findById(id).then((data) => res.json(data)).catch((error) => res.json({message: error}));
+});
+
+//obtener un producto
+
+router.get('/productos/:id', (req, res) => {
+    const { id } = req.params;
+    productSchema.findById(id).then((data) => res.json(data)).catch((error) => res.json({message: error}));
+});
+
+//actualizar un usuario
+
+router.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, age, email, adress } = req.body;
+    userSchema.updateOne({ _id: id}, { $set: {name, age, email, adress}}).then((data) => res.json(data)).catch((error) => res.json({message: error}));
+});
+
+//actualizar un producto
+
+router.get('/productos/:id', (req, res) => {
+    const { id } = req.params;
+    productSchema.findById(id).then((data) => res.json(data)).catch((error) => res.json({message: error}));
+});
 
 module.exports = router; 
